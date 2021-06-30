55ba618d94559003daff0d026c9e7cd34d216e14
Diego <pascual.diego7@gmail.com>
2021-06-30 14:46:32 +0200

    Fixing library

--- Frontend/projects/customizable-chat/src/public-api.ts
+++ Frontend/projects/customizable-chat/src/public-api.ts
@@ -3,13 +3,11 @@
  */
 
 export * from './lib/Customizable-Chat-Chatbox/customizable-chat-chatbox.service';
-
 export * from './lib/Customizable-Chat-Chatbox/customizable-chat-chatbox.component';
 export * from './lib/Customizable-Chat-Chatbox/image-dialog-content/image-dialog-content.component';
 export * from './lib/Customizable-Chat-Chatbox/customizable-chat-chatbox.module';
 
 export * from './lib/Customizable-Chat-Chatbox/Models/message';
-
 
 export * from './lib/Customizable-Chat-ChatList/customizable-chat-chat-list.component';
 export * from './lib/Customizable-Chat-ChatList/customizable-chat-chatlist.module';
