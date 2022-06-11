cb0848cce0702bd5d835dbefb2a4ad1223e14576
Diegopasfer1909 <pascual.diego7@gmail.com>
2022-06-11 18:10:50 +0200

    Update login.controller.ts

--- backend/src/Login/login.controller.ts
+++ backend/src/Login/login.controller.ts
@@ -1,35 +1,36 @@
-import {Body, Controller, Post, Put} from '@nestjs/common';
-import {UsersService} from "../Providers/users/users.service";
-import {CreateUserDto, LoginUserDTO} from "../DTOs/create-user-dto";
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post, Put } from '@nestjs/common';
+import { UsersService } from '../Providers/users/users.service';
+import { CreateUserDto, LoginUserDTO } from '../DTOs/create-user-dto';
 
 @Controller('sign')
 export class LoginController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
-    @Put('in')
-    async loginUser(@Body() user: LoginUserDTO){
-        let userPromise = await this.usersService.findOneByUsername(user.userName);
-        if(userPromise?.password === user.password){
-            return userPromise;
-        }
-        return null;
+  @Put('in')
+  async loginUser(@Body() user: LoginUserDTO) {
+    const userPromise = await this.usersService.findOneByUsername(user.userName);
+    if (userPromise?.password === user.password) {
+      return userPromise;
     }
+    return null;
+  }
 
-    @Post('up')
-    async signUp(@Body() userData: CreateUserDto){
-        let user = await this.usersService.findOneByUsername(userData.userName);
-        if(user === null) return await this.usersService.createUser(userData);
-        else return null;
-    }
+  @Post('up')
+  async signUp(@Body() userData: CreateUserDto) {
+    const user = await this.usersService.findOneByUsername(userData.userName);
+    if (!user) return await this.usersService.createUser(userData);
+    else return null;
+  }
 
-    @Put('out')
-    async logout(@Body() user:LoginUserDTO){
-        let data = await this.usersService.findOneByUsername(user.userName);
-        if( data != undefined && data.password == user.password){
-            let logoutResult =  await this.usersService.logout(user.userName);
-            this.usersService.findOneBySocketIdAndEraseActivity(data.socketId);
-            return logoutResult;
-        }
-        return false;
+  @Put('out')
+  async logout(@Body() user: LoginUserDTO) {
+    const data = await this.usersService.findOneByUsername(user.userName);
+    if (data && data.password === user.password) {
+      const logoutResult = await this.usersService.logout(user.userName);
+      this.usersService.findOneBySocketIdAndEraseActivity(data.socketId);
+      return logoutResult;
     }
+    return false;
+  }
 }
