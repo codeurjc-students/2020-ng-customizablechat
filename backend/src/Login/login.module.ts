2e0b1700d174f64a9d7dcdcfbd47555575cd73b7
Diegopasfer1909 <pascual.diego7@gmail.com>
2022-06-11 23:57:50 +0200

    Changes in login formatting

--- backend/src/Login/login.module.ts
+++ backend/src/Login/login.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import {LoginController} from './login.controller';
-import {MongooseModule} from "@nestjs/mongoose";
-import {UserSchema} from "../Schemas/user.schema";
-import {UsersService} from "../Providers/users/users.service";
+import { LoginController } from './login.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from '../Schemas/user.schema';
+import { UsersService } from '../Providers/users/users.service';
 
-
-@Module({imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [LoginController],
-  providers: [UsersService]})
+  providers: [UsersService],
+})
 export class LoginModule {}
--- backend/src/Users/users.controller.ts
+++ backend/src/Users/users.controller.ts
@@ -1,24 +1,23 @@
-import {Body, Controller, Get, Logger, Param, Put} from '@nestjs/common';
-import {UsersService} from "../Providers/users/users.service";
-import {SendChangeColor} from "../DTOs/create-user-dto";
-
+import { Body, Controller, Get, Logger, Param, Put } from '@nestjs/common';
+import { UsersService } from '../Providers/users/users.service';
+import { SendChangeColor } from '../DTOs/create-user-dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(
-        private readonly usersService: UsersService,
-    ) {}
+  constructor(private readonly usersService: UsersService) {}
 
-    private logger : Logger = new Logger('UsersController')
+  private logger: Logger = new Logger('UsersController');
 
-    @Put('color-modes')
-    async changeColorMode(@Body() change: SendChangeColor){
-        return await this.usersService.changeColorMode(change.userName,change.idSettings);
-    }
+  @Put('color-modes')
+  async changeColorMode(@Body() change: SendChangeColor) {
+    return await this.usersService.changeColorMode(
+      change.userName,
+      change.idSettings,
+    );
+  }
 
-    @Get('images/:userName')
-    async retrieveUserImage(@Param('userName') userName:String) {
-        return this.usersService.getImageProfile(userName);
-    }
-
+  @Get('images/:userName')
+  async retrieveUserImage(@Param('userName') userName: string) {
+    return this.usersService.getImageProfile(userName);
+  }
 }
